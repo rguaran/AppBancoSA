e744b1542acec09ce2759d91941a91d6c8fe3b61
rguaran <ritaguaran@gmail.com>
2015-06-25 11:17:08 -0600

    clase usuario

---
+++ BancoSA/src/java/control/Usuario.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rita
+ */
+public class Usuario {
+    String DPI;
+    String nombre;
+    String apellido;
+    String telefono;
+    String direccion;
+    String correo;
+    String nombreUsuario;
+    String contraseña;
+    String fechacreacion;
+    String confirmado;
+    ArrayList<Cuenta>  cuentas = new ArrayList<Cuenta>();
+
+    public Usuario() {
+    }
+
+    public String CrearCuenta(Cuenta nuevacuenta){
+        cuentas.add(nuevacuenta);
+        return "cuenta agregada";
+    }
+    
+    public String getDPI() {
+        return DPI;
+    }
+
+    public void setDPI(String DPI) {
+        this.DPI = DPI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getFechacreacion() {
+        return fechacreacion;
+    }
+
+    public void setFechacreacion(String fechacreacion) {
+        this.fechacreacion = fechacreacion;
+    }
+
+    public String getConfirmado() {
+        return confirmado;
+    }
+
+    public void setConfirmado(String confirmado) {
+        this.confirmado = confirmado;
+    }
+
+    public ArrayList getCuentas() {
+        return cuentas;
+    }
+
+    public void setCuentas(ArrayList<Cuenta> cuentas) {
+        this.cuentas = cuentas;
+    }
+
+    public Usuario(String DPI, String nombre, String apellido, String telefono, String direccion, String correo, String nombreUsuario, String contraseña, String fechacreacion, String confirmado, ArrayList<Cuenta> cuentas) {
+        this.DPI = DPI;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.correo = correo;
+        this.nombreUsuario = nombreUsuario;
+        this.contraseña = contraseña;
+        this.fechacreacion = fechacreacion;
+        this.confirmado = confirmado;
+        this.cuentas = cuentas;
+    }
+    
+    
+}
